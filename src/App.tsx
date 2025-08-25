514a75748e0df028e4b85288c3b18db75dca079c
Phumphut <phumphut.rueng@gmail.com>
2025-08-25 13:32:17 +0700

    feat : ex3

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import UsersList from "./pages/UserList";
 
 function App() {
   return (
     <Router>
       <Routes>
         // TODO3: เพิ่ม Route ไป Home ตรงนี้
-        <Route path="/" element={} />
+        <Route path="/" element={<Home name="John Doe" />} />
         // TODO4: เพิ่ม Route ไป UsersList ตรงนี้
-        <Route path="/users" element={} />
+        <Route path="/users" element={<UsersList />} />
       </Routes>
     </Router>
   );
